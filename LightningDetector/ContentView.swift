38a6d4e6c33dbab0c973f88dc16c824a9f3b309e
Benji <sluggerbebenjamin@gmail.com>
2026-06-20 21:25:53 -0400

    initial commit

---
+++ LightningDetector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LightningDetector
+//
+//  Created by Ben Titus on 6/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LightningDetector/LightningDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightningDetectorApp.swift
+//  LightningDetector
+//
+//  Created by Ben Titus on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct LightningDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
